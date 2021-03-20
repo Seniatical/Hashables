44f434dcc618fefa9f8e73cc55cdb0ef106473d3
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-03-20 17:55:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as file:
-    desc = file.read()
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: Apache License',
+  'Programming Language :: Python :: 3'
+]
 
-setuptools.setup(
-  
-    name = "hashize",
-    version = "0.0.1",
-    author = "Seniatical",
-    license = "MIT",
-  
-    description = "Rewrite of the default python datatypes",
-    long_description = desc,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/Seniatical/Hashize",
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        'Intended Audience :: Developers',
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        'Topic :: Utilities',
-
-    ],
+setup(
+  name='MechaK.py',
+  version='0.0.1',
+  description='Python Data Types Rewritten!',
+    
+  long_description=open('README.md', 'r', encoding='utf-8').read(),
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/Seniatical/Hashize/", 
+    
+  project_urls={
+   "Homepage": "https://github.com/Seniatical/MechaK.py/",
+   },
+    
+  author='Seniatical',
+  license='Apache', 
+  classifiers=classifiers,
+  keywords='API,Wrapper', 
+  packages=find_packages(),
+  install_requires=[''] 
 )
